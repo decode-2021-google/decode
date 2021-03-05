185fa7821bc51e8d544d8abdf1952b27e26075c7
Areena Akhter <akhterareena@gmail.com>
2021-03-05 16:04:29 -0500

    Added bottom nav

--- frontend/src/components/bottomNav.js
+++ frontend/src/components/bottomNav.js
@@ -2,19 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { DarkModeSwitch } from "./darkmodeSwitch";
 import { RelaxNav } from "./primaryButton";
+import { ToDoListIcon } from "./toDoListIcon";
 
-const BottomNavStyles = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const BottomNav = () => {
-  return (
-    <TopNavStyles>
-      <DarkModeSwitch />
-      <ProfileSettings />
-    </TopNavStyles>
-  );
-};
-
+BottomNav = () => {
+    switch(window.location.pathname){
+      case '/relax':
+        return RelaxNav;
+      default:
+        return ToDoListIcon;
+    }
+}
 export { BottomNav };
