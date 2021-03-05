7c278d6d8e87ac215a358f4130f20fd727994cad
Jacky Chiu <jackyzhanchiu@gmail.com>
2021-03-05 00:03:41 +0000

    refactor to work with app engine

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
 
-@APP.route('/test/get', methods=['GET'])
+@app.route('/test/get', methods=['GET'])
 def search_form():
     return jsonify({'text': 'successful'}), 200
 
 
 if __name__ == '__main__':
-    APP.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
