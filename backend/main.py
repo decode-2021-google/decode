19cdbf6823c2989dff275624dd4bb730dd71693c
Tim Cosby <tim.cosby0@gmail.com>
2021-03-04 19:43:17 -0500

    Added video length to the embedded youtube endpoint.

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-embedded_youtube_video_template = '<iframe width="{width}" height="{height}" src="https://www.youtube.com/embed/{youtube_code}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+embedded_youtube_video_template = '<iframe width="{width}" height="{height}" src="https://www.youtube.com/embed/{video_code}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
 
 @app.route('/test/get', methods=['GET'])
@@ -12,7 +12,7 @@ def search_form():
 
 
 def get_random_youtube_code(category):
-    return '5qap5aO4i9A'
+    return {'video_code': '5qap5aO4i9A', 'video_length': 0}
 
 
 @app.route('/embedded/youtube', methods=['GET'])
@@ -22,9 +22,9 @@ def get_youtube_embed():
     video_width = request.args.get('video_width', default='560')
     video_height = request.args.get('video_height', default='315')
 
-    youtube_code = get_random_youtube_code(category)
+    youtube_dict = get_random_youtube_code(category)
 
-    return jsonify({'embed_link': embedded_youtube_video_template.format(width=video_width, height=video_height, youtube_code=youtube_code)})
+    return jsonify({'embed_link': embedded_youtube_video_template.format(width=video_width, height=video_height, video_code=youtube_dict['video_code']), 'video_length': youtube_dict['video_length']}), 200
 
 
 if __name__ == '__main__':
