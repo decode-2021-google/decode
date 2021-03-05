62b36076139b3e3afa97cd5f1d76206ad6c7f0c1
Areena Akhter <akhterareena@gmail.com>
2021-03-05 18:59:13 -0500

    styling for to-do

--- frontend/src/components/taskList.js
+++ frontend/src/components/taskList.js
@@ -4,19 +4,23 @@ import { Task } from "./tasks";
 
 const TaskListStyles = styled.div`
   .listBox {
-    display: flex;
     position: absolute;
-    width: 500px;
-    height: 50px;
-    bottom: 1rem;
-    left: 1rem;
+    width: 300px;
+    height: 300px;
+    bottom: 6rem;
+    left: 2rem;
     background-color: rgba(136, 153, 172, 0.7);
     border-radius: 30px;
 
     .listHeader {
+      width: 80%;
       padding: 1rem 2rem 1rem 2rem;
       display:flex;
       justify-content: space-between;
+    }
+
+    .listHeader h3{
+      color: white;
     }
 
     .tasks {
@@ -30,13 +34,13 @@ const TaskList = () => {
     <TaskListStyles>
       <div className="listBox">
         <div className="listHeader">
-          <p>Add new task</p> 
-          <p>+</p>
+          <h3>Add new task</h3> 
+          <h3>+</h3>
         </div>
-        <div className="tasks">
-          <Task>First Task</Task>
-          <Task>First Task</Task>
-          <Task>First Task</Task>
+        <div className="btn-container">
+          <Task>Clean room </Task>
+          <Task>Finish coding</Task>
+          <Task>Practice presentation</Task>
         </div>
       </div>
     </TaskListStyles>
--- frontend/src/components/tasks.js
+++ frontend/src/components/tasks.js
@@ -2,8 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const TaskStyles = styled.button`
+  width: 80%;
+  display: block;
+  justify-content: center;
   background-color: rgba(255, 255, 255, 0.3);
-  padding: 1rem 2rem;
+  margin: 20px 20px 20px 20px;
+  padding: 1rem 2rem 1rem 2rem;
   border-radius: 1.2rem;
   color: white;
   font-weight: 700;
