50bcbe6537f45da29ccf51fb45c746a806d1d9da
Omegashin <gdesigner.arshdeep@gmail.com>
2021-03-05 13:52:45 -0500

    feat: added authentication
    For now, the profile button is used for Sign Out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
-
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
+
+//auth
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+import { firebaseConfig } from "./firebase-config";
 
 //pages
 import { Welcome } from "./pages/welcome";
@@ -13,26 +18,29 @@ import { NotFound } from "./pages/notFound";
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" exact>
-          <Welcome />
-        </Route>
-        <Route path="/work" exact>
-          <Work />
-        </Route>
-        <Route path="/break" exact>
-          <Break />
-        </Route>
-        <Route path="/categories" exact>
-          <Categories />
-        </Route>
-        <Route path="/relax" exact>
-          <Relax />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+        <Switch>
+          <Route path="/" exact>
+            <Welcome/>
+            <Work/>
+          </Route>
+          <Route path="/work" exact>
+            <Work />
+          </Route>
+          <Route path="/break" exact>
+            <Break />
+          </Route>
+          <Route path="/categories" exact>
+            <Categories />
+          </Route>
+          <Route path="/relax" exact>
+            <Relax />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </FirebaseAuthProvider>
     </div>
   );
 }
