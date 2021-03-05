67d4c99b31abd3e90bfcb279d70721f7bcfd86be
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-04 22:14:16 -0500

    navigation styling complete

--- frontend/src/components/darkmodeSwitch.js
+++ frontend/src/components/darkmodeSwitch.js
@@ -2,13 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const DarkModeSwitchStyles = styled.div`
-  width: 5rem;
-  height: 5rem;
-  border: 1px solid black;
+  width: 2.5rem;
+  height: 1.4rem;
+  border: 0.2rem solid white;
+  border-radius: 20rem;
+  position: relative;
+  cursor: pointer;
+  .dot {
+    width: 0.8rem;
+    height: 0.8rem;
+    background-color: white;
+    border-radius: 100%;
+    position: absolute;
+    top: 50%;
+    left: 0.2rem;
+    transform: translateY(-50%);
+  }
 `;
 
 const DarkModeSwitch = () => {
-  return <DarkModeSwitchStyles>darkmode</DarkModeSwitchStyles>;
+  return (
+    <DarkModeSwitchStyles>
+      <div className="dot"></div>
+    </DarkModeSwitchStyles>
+  );
 };
 
 export { DarkModeSwitch };
