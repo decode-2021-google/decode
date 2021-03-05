68fc7c6b18600eafb077cc8f126539a8a6ca8d9c
Jacky Chiu <jackyzhanchiu@gmail.com>
2021-03-05 00:17:04 +0000

    test auto deploy

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/test/get', methods=['GET'])
 def search_form():
-    return jsonify({'text': 'successful'}), 200
-
+  return jsonify({'text': 'successful', 
+                  'auto_deploy': 'successful'}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
