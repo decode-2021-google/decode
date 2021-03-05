3fc0e29058b37c811c70d081322b5212313e77b0
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-05 17:48:24 -0500

    adding placeholder video for relax page

--- frontend/src/pages/relax.js
+++ frontend/src/pages/relax.js
@@ -5,19 +5,37 @@ import { ToDoListIcon } from "../components/toDoListIcon";
 import { TopNav } from "../components/topNav";
 
 const RelaxStyles = styled.div`
-  .content {
-    margin: 2rem auto;
-    width: 80vw;
-    height: 70vh;
-    border: 1px solid red;
+  .container {
+    text-align: center;
+    iframe {
+      margin: 2rem auto;
+      width: 75vw;
+      height: 70vh;
+      border: none;
+    }
   }
 `;
 
 const Relax = (props) => {
   return (
     <RelaxStyles>
-      <TopNav user={props.user} isSignedIn={props.isSignedIn}/>
-      <div className="content"></div>
+      <TopNav user={props.user} isSignedIn={props.isSignedIn} />
+      <div className="container">
+        <iframe
+          className="content"
+          title="video"
+          src="https://www.youtube.com/embed/xvFZjo5PgG0?controls=0"
+          allowfullscreen
+        ></iframe>
+      </div>
+      {/* <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/xvFZjo5PgG0?controls=0"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> */}
       <RelaxNav />
     </RelaxStyles>
   );
