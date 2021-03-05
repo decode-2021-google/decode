6b33bfc05e55d1f5f95c9c32285777138b1c8bb1
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-05 11:43:46 -0500

    adding minor hover animation for button

--- frontend/src/components/primaryButton.js
+++ frontend/src/components/primaryButton.js
@@ -2,12 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const PrimaryButtonStyles = styled.button`
-  background: rgba(255, 255, 255, 0.3);
+  background-color: rgba(255, 255, 255, 0.3);
   padding: 1rem 2rem;
   border-radius: 1.2rem;
   color: white;
   font-weight: 700;
   font-size: 1rem;
+  cursor: pointer;
+  transition: 0.3s all ease 0.1s;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.5);
+    transition: 0.3s all ease 0.1s;
+  }
 `;
 
 const PrimaryButton = (props) => {
