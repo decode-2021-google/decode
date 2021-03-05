d104b927b7f61e8c11c2f99cdb0c1adad5e48800
Tim Cosby <tim.cosby0@gmail.com>
2021-03-04 21:19:53 -0500

    Implemented front end feedback.

--- backend/embedded_templates.py
+++ backend/embedded_templates.py
@@ -1,44 +1,4 @@
-embedded_youtube_video_template = \
-    '''
-        <iframe 
-            width="{width}" 
-            height="{height}" 
-            src="https://www.youtube.com/embed/{video_code}" 
-            frameborder="0" 
-            allow="accelerometer; 
-            autoplay; 
-            clipboard-write; 
-            encrypted-media; 
-            gyroscope; 
-            picture-in-picture" 
-            allowfullscreen>
-        </iframe>
-    '''
-embedded_rpan_video_template = '<video tabindex="-1" src="blob:https://www.reddit.com/fc8c6a17-edea-495c-b271-85877c483d14"></video>'  # No clue if this is real
-embedded_twitch_channel_template = \
-    '''
-        <iframe
-            src="https://player.twitch.tv/?channel={channel}&parent=google.com"
-            height="{height}"
-            width="{width}"
-            allowfullscreen="false">
-        </iframe>
-    '''
-embedded_twitch_video_template = \
-    '''
-        <iframe
-            src="https://player.twitch.tv/?video={video_code}&parent=google.com"
-            height="{height}"
-            width="{width}"
-            allowfullscreen="false">
-        </iframe>
-    '''
-embedded_twitch_clip_template = \
-    '''
-        <iframe
-            src="https://clips.twitch.tv/embed?clip={video_code}&parent=google.com"
-            height="{height}"
-            width="{width}"
-            allowfullscreen="false">
-        </iframe>
-    '''
+embedded_youtube_video_template = 'https://www.youtube.com/embed/{video_code}'
+embedded_twitch_channel_template = 'https://player.twitch.tv/?channel={channel}&parent=google.com'
+embedded_twitch_video_template = 'https://player.twitch.tv/?video={video_code}&parent=google.com'
+embedded_twitch_clip_template = 'https://clips.twitch.tv/embed?clip={video_code}&parent=google.com'
--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,6 @@ def test_get():
     return jsonify({'text': 'successful'}), 200
 
 
-def clean_template(template):
-    return template.replace(' ', '').replace('\n', '')
-
-
 def get_random_youtube_code(category):
     return {'video_code': '5qap5aO4i9A', 'video_length': 0}
 
@@ -22,13 +18,9 @@ def get_random_youtube_code(category):
 def get_youtube_embed():
     category = request.args.get('category', default='random')
 
-    video_width = request.args.get('video_width', default='560')
-    video_height = request.args.get('video_height', default='315')
-
     youtube_dict = get_random_youtube_code(category)
 
-    video_template = clean_template(embedded_youtube_video_template)
-    embed_link = video_template.format(width=video_width, height=video_height, video_code=youtube_dict['video_code'])
+    embed_link = embedded_youtube_video_template.format(video_code=youtube_dict['video_code'])
 
     return jsonify({'embed_link': embed_link, 'video_length': youtube_dict['video_length']}), 200
 
@@ -49,9 +41,6 @@ def get_random_twitch_clip(category):
 def get_twitch_embed():
     category = request.args.get('category', default='random')
 
-    video_width = request.args.get('video_width', default='560')
-    video_height = request.args.get('video_height', default='315')
-
     get_twitch_clip = True  # TODO: Create some randomness later
 
     # Might not want twitch streams since it's a larger requirement to see if they are online
@@ -63,14 +52,12 @@ def get_twitch_embed():
     if get_twitch_clip:
         twitch_dict = get_random_twitch_clip(category)
 
-        clip_template = clean_template(embedded_twitch_clip_template)
-        embed_link = clip_template.format(width=video_width, height=video_height, video_code=twitch_dict['video_code'])
+        embed_link = embedded_twitch_clip_template.format(video_code=twitch_dict['video_code'])
         video_length = twitch_dict['video_length']
     else:
         twitch_dict = get_random_twitch_video(category)
 
-        video_template = clean_template(embedded_twitch_video_template)
-        embed_link = video_template.format(width=video_width, height=video_height, video_code=twitch_dict['video_code'])
+        embed_link = embedded_twitch_video_template.format(video_code=twitch_dict['video_code'])
         video_length = twitch_dict['video_length']
 
     return jsonify({'embed_link': embed_link, 'video_length': video_length}), 200
