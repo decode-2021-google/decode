e74d93f9080c69a84557ba2de773eaa960c1eab8
Areena Akhter <akhterareena@gmail.com>
2021-03-05 14:48:00 -0500

    Adding break footer

--- frontend/src/pages/break.js
+++ frontend/src/pages/break.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { TopNav } from "../components/topNav";
+import { ToDoListIcon } from "../components/toDoListIcon";
 
 const BreakStyles = styled.div``;
 
@@ -9,6 +10,7 @@ const Break = () => {
     <BreakStyles>
       <TopNav />
       Break page
+      <ToDoListIcon />
     </BreakStyles>
   );
 };
